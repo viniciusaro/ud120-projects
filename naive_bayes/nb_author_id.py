6f0ac50dbf72d01aa568ff7e8f69d1732fd82320
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2017-07-30 14:55:07 -0300

    SVM

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,11 +23,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "tempo de treinamento:", round(time()-t0, 3), "s"
 
+print clf.score(features_test, labels_test)
 #########################################################
-
-
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,33 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(C=10000.0, kernel="rbf")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "tempo de treinamento:", round(time()-t0, 3), "s"
 
+# t1 = time()
+pred = clf.predict(features_test)
+# print "tempo de predicao:", round(time()-t1, 3), "s"
+
+count = 0
+for prediction in pred:
+	if prediction == 1:
+		count += 1
+
+print "count: ", count
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 #########################################################
 
-
+# tempo de treinamento: 178.093 s
+# tempo de predicao: 17.146 s
+# 0.984072810011
